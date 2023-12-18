8b458233a287feb0b8fc3c9f6cee8c360d66523d
TheMilkMan <jerome.lagacecarter000@sccdsb.net>
2023-12-18 09:22:42 -0500

    Update reindeer.py

--- reindeer.py
+++ reindeer.py
@@ -2,12 +2,12 @@
 # Description: Santa's Reindeer
 
 
-santas_reindeer = [ "Rudolph",
-                   "Dasher",
-                   "Danser",
-                   "Prancer",
-                   "Vixen",
-                   "Comet",
-                   "Cupid",
-                   "Donder",
-                   "Blitzen" ]
+santas_reindeer = ["rudolph",
+                   "dasher",
+                   "danser",
+                   "prancer",
+                   "vixen",
+                   "comet",
+                   "cupid",
+                   "donder",
+                   "blitzen" ]
